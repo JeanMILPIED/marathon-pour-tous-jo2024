6202bc7306833ec04c3c21393661abb6497434b6
JEAN <56645415+JeanMILPIED@users.noreply.github.com>
2024-08-16 07:54:48 +0200

    Update JO-marathon.py

--- JO-marathon.py
+++ JO-marathon.py
@@ -37,9 +37,9 @@ if submit_button:
     participant = get_rank(bib_number)
     if participant is not None:
         try:
-            rank_net=int(participant['rank_net'].values[0]))
+            rank_net=int(participant['rank_net'].values[0])
             top_net=int(rank_net/20136*100)
-            rank_brut=int(participant['rank_brut'].values[0]))
+            rank_brut=int(participant['rank_brut'].values[0])
             top_brut=int(rank_brut/20136*100)
             st.success('Based on your **net time**, Your rank is **{}** on 20136 registered participants (Top {}%)'.format(rank_net, top_net)
             st.success('Based on your **brut time**, Your rank is **{}** on 20136 registered participants (Top {}%)'.format(rank_brut, top_brut)
